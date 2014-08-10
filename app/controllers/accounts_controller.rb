c5338c030467c2d530448f03f470f4bc3e5c9288
Ivan Dimitrov <idimitrov07@gmail.com>
2014-08-11 00:29:11 +0300

    AcriveRecord example

---
+++ app/controllers/accounts_controller.rb
@@ -0,0 +1,11 @@
+class AccountsController < ApplicationController
+
+	def index
+		@accounts = Customer.all
+	end
+
+	def show
+		@account = Account.find(params[:id])
+	end
+
+end
---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,12 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key is used for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+# You can use `rake secret` to generate a secure secret key.
+
+# Make sure your secret_key_base is kept private
+# if you're sharing your code publicly.
+Billinger::Application.config.secret_key_base = '88b3286ed50e4360c5dc74c8b45b06a717b5eb513d7442085ad27c53b6cf8a8bbcdd2f9115613125d90936b4ea913d8f918abd4b973f987ca5e0ccd942773d58'
